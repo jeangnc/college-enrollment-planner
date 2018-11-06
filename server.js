53484e046a32073374cca8c737ac93d776b5fbbd
Jean Gonçalves <jean.gnc@gmail.com>
2018-11-06 20:56:18 -0200

    Use static files serving instead of dump root route on express

--- server.js
+++ server.js
@@ -1,22 +1,13 @@
 const path = require('path');
 const express = require('express');
-const webpack = require('webpack');
-const webpackDevMiddleware = require('webpack-dev-middleware');
-const config = require('./webpack.config.js');
 
 const PORT = process.env.PORT || 3000
-
 const app = express();
-const compiler = webpack(config);
 
-// Tell express to use the webpack-dev-middleware and use the webpack.config.js
-// configuration file as a base.
-app.use(webpackDevMiddleware(compiler, {
-  publicPath: config.output.publicPath
-}));
+app.use('/', express.static(path.join(__dirname + '/dist')))
 
 // Serve the files on port 3000.
 app.listen(PORT, () => {
   console.log(`App listening to ${PORT}....`)
-  console.log("Press Ctrl+C to quit.\n")
+  console.log("Press Ctrl+C to quit.")
 })
