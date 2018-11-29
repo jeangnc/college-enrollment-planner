83e60ec2c674f2cd233f4e7d62c70e1f697a93c8
Jean Gonçalves <jean.gnc@gmail.com>
2018-11-29 19:58:00 -0200

    Add app component

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import AllSemesterList from './containers/AllSemesterList';
+
+const App = (props) => {
+  return (
+    <div class="app">
+      <AllSemesterList />
+    </div>
+  );
+}
+
+export default App;
