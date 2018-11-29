5df4511fcc15645ee425ad06a6ce655c40fd8301
Jean Gonçalves <jean.gnc@gmail.com>
2018-11-29 19:56:00 -0200

    Add CSS loader to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,13 @@ module.exports = {
         ],
       },
       {
+        test: /\.css$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" },
+        ],
+      },
+      {
         test: /\.js$/,
         exclude: /node_modules/,
         use: { loader: "babel-loader" },
